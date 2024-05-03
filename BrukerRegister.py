db0f94c54e91cb62792f79c90db4de2f43e6607b
KarstenSnapa <karsten.snapa@gmail.com>
2024-05-03 10:01:57 +0200

    Lagt til en funksjon som lagde en adminer bruker, jeg trenger ikke denne funksjonen lenger men jeg trengte å ha en adminer bruker i databasen for å jobbe videre

--- BrukerRegister.py
+++ BrukerRegister.py
@@ -7,4 +7,14 @@ connection = pymysql.connect(host='172.20.128.79',
                             charset='utf8mb4',
                             cursorclass=pymysql.cursors.DictCursor)
 
+adminer_navn = 'adminer'
+adminer_passord = 'passord'
+adminer_tilgang = 'admin'
 
+
+def create_adminer(adminer_navn, adminer_passord, adminer_tilgang):
+    with connection.cursor() as cursor:
+        cursor.execute("INSERT INTO Liste (navn, passord, tilgang) VALUES (%s, %s, %s)", (adminer_navn, adminer_passord, adminer_tilgang))
+    connection.commit()
+
+create_adminer(adminer_navn, adminer_passord, adminer_tilgang)
