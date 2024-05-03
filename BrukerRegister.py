693c0f6bf35ef71eae1e5c9cb6446ef4012734a5
KarstenSnapa <karsten.snapa@gmail.com>
2024-05-03 10:27:26 +0200

    lar deg logge inn på brukere med brukernavn og passord, foreløpig har jeg bare en adminer bruker med navnet "adminer" og passordet "passord"

--- BrukerRegister.py
+++ BrukerRegister.py
@@ -7,14 +7,45 @@ connection = pymysql.connect(host='172.20.128.79',
                             charset='utf8mb4',
                             cursorclass=pymysql.cursors.DictCursor)
 
-adminer_navn = 'adminer'
-adminer_passord = 'passord'
-adminer_tilgang = 'admin'
+bruker_login = ''
+passord_login = ''
+forsøk_passord = 10
 
-
-def create_adminer(adminer_navn, adminer_passord, adminer_tilgang):
+def les_bruker(bruker_login):
     with connection.cursor() as cursor:
-        cursor.execute("INSERT INTO Liste (navn, passord, tilgang) VALUES (%s, %s, %s)", (adminer_navn, adminer_passord, adminer_tilgang))
-    connection.commit()
+        cursor.execute("SELECT * FROM Liste WHERE Navn = %s", (bruker_login))
+        navn_liste = cursor.fetchone()
+    if navn_liste and bruker_login in navn_liste['Navn']:
+        print("bruker finnes")
+        print("Skriv passord")
+        passord_login = input()
+        les_passord(passord_login)
+    else:
+        print("bruker ikke tilgjengelig")
+        les_bruker(bruker_login)
 
-create_adminer(adminer_navn, adminer_passord, adminer_tilgang)
+
+def les_passord(passord_login):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Liste WHERE Passord = %s", (passord_login))
+        passord_liste = cursor.fetchone()
+    if passord_liste and passord_login in passord_liste['Passord']:
+        print("du er logget inn!")
+    else:
+        print("feil passord")
+        print(forsøk_passord, "forsøk igjen på riktig passord")
+        forsøk_passord -= 1
+        if forsøk_passord > 0:
+            les_passord()
+        else:
+            print("Du er tom for forsøk!")
+
+
+def login():
+    forsøk_passord = 10
+    print("logg inn med epost eller navn:")
+    bruker_login = input()
+    les_bruker(bruker_login)
+
+
+login()
